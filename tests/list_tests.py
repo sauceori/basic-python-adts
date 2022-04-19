d204cf27dbd211d1732fbcfcec077c4f292a4a04
Ethan Hopkins <hopkinsem99@gmail.com>
2022-04-19 12:09:33 -0400

    Unit testing skeleton for list ADT

---
+++ tests/list_tests.py
@@ -0,0 +1,50 @@
+"""Unit tests for the linked-list data structure in the challenge module.
+
+list_tests contains test cases for the methods belonging to the list class,
+which provide a public interface for the linked list data structure used to
+add or remove nodes, find and erase nodes, and determine whether the list is
+empty among other uses.
+"""
+
+import unittest
+from list import list
+
+class ListTester(unittest.TestCase):
+    """Contains the unit tests for the public interface of a linked list.
+    """
+
+    def test_push_front(self):
+        pass
+
+    def test_top_front(self):
+        pass
+
+    def test_pop_front(self):
+        pass
+
+    def test_push_back(self):
+        pass
+
+    def test_top_back(self):
+        pass
+
+    def test_pop_back(self):
+        pass
+
+    def test_find(self):
+        pass
+
+    def test_erase(self):
+        pass
+
+    def test_empty(self):
+        pass
+
+    def test_add_before(self):
+        pass
+
+    def test_add_after(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#Simple script to execute all my test cases
+
+python -m unittest list_tests
+#python -m unittest stack_tests
+#python -m unittest queue_tests
