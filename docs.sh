1de3bd90dd37a9c5fa8eb22b43fce3b738421005
Ethan Hopkins <hopkinsem99@gmail.com>
2022-04-19 23:41:46 -0400

    Add a script to generate html docs

---
+++ docs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#Simple script to generate all my docs and move them to the appropriate folder
+
+pydoc -w adts/list.py
+#pydoc -w adts/stack.py
+#pydoc -w adts/queue.py
+
+pydoc -w tests/list_tests.py
+#pydoc -w tests/stack_tests.py
+#pydoc -w tests/queue_tests.py
+
+mv *.html docs
